d95b3a1ed686dfff6ad1cb2827378519f55fcd1f
Wagner Cunha Amengual <wagner.cunha@hotmail.com>
2025-12-05 17:29:51 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/inventario', inventarioRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.send("Servidor rodando corretamente 🚀");
