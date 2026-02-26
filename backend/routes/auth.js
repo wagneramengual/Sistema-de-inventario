db49eed7642b83ca0facb90a13d4ddf0f67acea9
Wagner Cunha Amengual <wagner.cunha@hotmail.com>
2026-02-26 16:09:17 -0300

    Delete backend directory

--- backend/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.get('/usuarios', authController.listarUsuarios);
-router.post('/login', authController.login);
-
-module.exports = router;
--- backend/routes/inventario.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const inventarioController = require('../controllers/inventarioController');
-
-router.get('/:centro', inventarioController.getInventarioByCentro);
-
-module.exports = router;
